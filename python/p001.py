2dc31de5b7e4c24690957b215f75c22a5798598f
Sami Hakani <sami.hakani@gmail.com>
2023-01-30 08:56:37 -0500

    Faster way to solve problem 1

--- python/p001.py
+++ python/p001.py
@@ -4,9 +4,19 @@
 # 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+'''
+# old way; slow
 s = 0
 for i in range(1000):
     if i%3 == 0 or i%5 == 0:
         s += i
 
 print(s)
+'''
+
+# multiples of 3 below 1000:
+# 3 + 6 + ... + 999 = 3(1 + 2 ... + 333) = 3(1 + 2 + ... + 1000//3)
+# multiples of 5 below 1000:
+# 5 + 10 + ... + 1000 = 5(1 + 2 + ... + 200) = 5(1 + 2 + ... + 1000//5)
+
+print(int(3*(1000//3)*(1000//3 + 1)/2 + 5*(1000//5)*(1000//5 + 1)/2))
