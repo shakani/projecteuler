33c315d0f54f1b2a42bf6641bd9d32ac458c7395
Sami Hakani <sami.hakani@gmail.com>
2023-02-11 14:26:15 -0500

    Solved problem 76

---
+++ python/p076.py
@@ -0,0 +1,22 @@
+'''
+It is possible to write five as a sum in exactly six different ways:
+
+4 + 1
+3 + 2
+3 + 1 + 1
+2 + 2 + 1
+2 + 1 + 1 + 1
+1 + 1 + 1 + 1 + 1
+
+How many different ways can one hundred be written as a sum of at least two positive integers?
+
+'''
+
+def partitions(n):
+    parts = [1]+[0]*n
+    for t in range(1, n+1):
+        for i, x in enumerate(range(t, n+1)):
+            parts[x] += parts[i]
+    return parts[n] - 1 # don't count trivial partition
+
+print(partitions(100))
