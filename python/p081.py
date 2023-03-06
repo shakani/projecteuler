81f77b0be61f4dd3b2654dfcecb4534d099a9b21
Sami Hakani <sami.hakani@gmail.com>
2023-03-06 13:10:14 -0500

    Solved problem 81

--- python/p081.py
+++ python/p081.py
@@ -8,21 +8,43 @@ with open("p081-data.txt", 'r') as f:
     for i in range(80):
         data[i] = list(map(int, f.readline().strip('\n').split(',')))
 
-def min_shape(arr): 
+def get_diag(mat, i): # gets i-th diagonal of matrix
+    if i < len(mat):
+        return [mat[i-j][j] for j in range(i+1)]
+    else:
+        return [mat[len(mat)-1-j][i-len(mat)+1+j] for j in range(len(mat)-1 - (i%len(mat)))]
+
+def min_shape(arr): # takes arr of length N and returns arr of length (N-1) where elements are the pairwise minima of arr
     return [min(arr[i], arr[i+1]) for i in range(len(arr)-1)]
 
-def feed_forward(arr1, arr2): # arr 2 is longer
-    if len(arr1) > len(arr2):
-        return feed_forward(arr2, arr1)
-    else:
-        return arr1 + min_shape(arr2)
+def max_shape(arr): # similar to min_shape, but takes arr of length N and returns arr of length (N+1) where pairwise min of boundaries is trivial
+    return [arr[0]] + min_shape(arr) + [arr[-1]]
 
-def path_sum(mat):
-    mat = np.fliplr(mat)
-    N = int(mat.size**0.5)
-    answer = np.array([0])
-    for i in range(1-N, N):
-        #print(np.diag(mat, i), np.diag(mat, i+1), i)
-        print(feed_forward(np.diag(mat, i), np.diag(mat, i+1)))
+def add_diags(d1, d2): # adds d1 to d2 in the right way
+    if len(d1) > len(d2): # d1 is longer; cast to min shape
+        return [min_shape(d1)[i] + d2[i] for i in range(len(d2))]
+    else: # d1 is shorter; cast to max shape
+        return [max_shape(d1)[i] + d2[i] for i in range(len(d2))]
 
-path_sum(test)
+'''
+For M as an N x N matrix, computes minimal path sum from M_11 to M_NN by computing minimal path sum to middle diagonal.
+Middle diagonal is taken to be matrix elements M_XY where X+Y = N+1.
+Minimal distance minimizes distance from M_11 to middle diagonal + M_NN to middle diagonal. 
+
+Time complexity looks like O(N) for loop in the path_sum.
+Space complexity is O(N) < O(N^2) for reading in the matrix, so we're good here. 
+
+Probably worse than this if my intermediate methods above aren't optimized. 
+
+'''
+def path_sum(arr): # computes min distance to middle diagonal from M_11 
+    ans_A, ans_B = get_diag(arr, 0), get_diag(arr, 2*len(arr)-2)
+    for i in range(len(arr)-1):
+        a = get_diag(arr, i+1)
+        b = get_diag(arr, 2*len(arr)-3 - i)
+        ans_A, ans_B = add_diags(ans_A, a), add_diags(ans_B, b)
+
+    ans = [ans_A[i] + ans_B[i] - get_diag(arr, len(arr)-1)[i] for i in range(len(ans_A))]
+    return int(min(ans))
+
+print(path_sum(data))
